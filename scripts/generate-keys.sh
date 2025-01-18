12acb3010c060f452d5fbfacc933fa4df4b48670
Michal <m.nakiewicz@gmail.com>
2025-01-18 18:26:28 +0000

    SOPS file creation

--- scripts/generate-keys.sh
+++ scripts/generate-keys.sh
@@ -20,7 +20,12 @@ creation_rules:
   - path_regex: secrets/.*\.yaml$
     age: $(grep "public key:" ~/.sops/keys.txt | cut -d: -f2 | tr -d ' ')
 EOL
-
-echo "Keys generated and .sops.yaml created"
+echo SOPS file created at .sops.yaml
+echo "-----------------------------------"
+cat .sops.yaml
+echo "-----------------------------------"
+echo ""
 echo "IMPORTANT: Add the private key to GitHub Secrets as SOPS_AGE_KEY:"
+echo "-----------------------------------"
 cat ~/.sops/keys.txt
+echo "-----------------------------------"
