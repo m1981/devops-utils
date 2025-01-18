fb0a38e330e699b7da76ba195b5224ff4fd790bf
Michal <m.nakiewicz@gmail.com>
2025-01-18 16:34:10 +0000

    Age dev container to setup age-keygen for encrypt secrets

---
+++ scripts/build-and-test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eux
+
+echo "Building all utility containers..."
+# Get host user's UID and GID
+HOST_UID=$(id -u)
+HOST_GID=$(id -g)
+
+# Build the image
+docker build \
+  --build-arg USER_ID=$HOST_UID \
+  --build-arg GROUP_ID=$HOST_GID \
+  -t age-dev docker/age-sops
+
+## Test the setup
+#docker run -it --rm \
+#  -v $(pwd):/home/developer/work \
+#  age-dev bash -ce '
+#    /usr/local/bin/check-age-environment.sh
+#  '
+
+#todo: Add Terraform build and test
+#docker build -t terraform-workspace docker/terraform
--- scripts/generate-keys.sh
+++ scripts/generate-keys.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-# TODO: Add script logic
+set -eu
+
+# Create directories
+mkdir -p ~/.sops
+mkdir -p secrets
+
+# Generate Age key pair
+docker run --rm -it \
+  -v ~/.sops:/home/developer/.sops \
+  age-dev bash -c '
+    age-keygen -o ~/.sops/keys.txt
+    echo "Public key:"
+    cat ~/.sops/keys.txt | grep "public key"
+  '
+
+# Create .sops.yaml configuration
+cat > .sops.yaml << EOL
+creation_rules:
+  - path_regex: secrets/.*\.yaml$
+    age: $(grep "public key:" ~/.sops/keys.txt | cut -d: -f2 | tr -d ' ')
+EOL
+
+echo "Keys generated and .sops.yaml created"
+echo "IMPORTANT: Add the private key to GitHub Secrets as SOPS_AGE_KEY:"
+cat ~/.sops/keys.txt
